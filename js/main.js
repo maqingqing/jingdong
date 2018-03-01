bcb65f061131ff96d1aaf4977585c5ced5741e19
maqingqing <751747880@qq.com>
2018-03-01 15:53:46 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+//	设置banner轮播
+	var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop:true,
+//    autoplay: {
+//      delay: 2500,
+//      disableOnInteraction: false,
+//    },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+    
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop:true,
+//    autoplay: {
+//      delay: 2500,
+//      disableOnInteraction: false,
+//    },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+})
